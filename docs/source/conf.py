cc0e9d162c171a59debb9f41c4772738ada0e374
ilestis <jay.payne.ch@gmail.com>
2024-01-21 08:12:55 -0600

    Favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,14 +27,17 @@ source_suffix = [
 # https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
 html_theme_options = {
     'analytics_id': 'G-Q3YPF8N1J0',
-    'analytics_anonymize_ip': True
+    'analytics_anonymize_ip': True,
+    'logo_only': True,
+    'navigation_depth': 5,
 }
 html_logo = 'img/kanka-white.png'
+html_favicon = "img/favicon.ico"
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
+# intersphinx_mapping = {
+#     'python': ('https://docs.python.org/3/', None),
+#     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+# }
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
